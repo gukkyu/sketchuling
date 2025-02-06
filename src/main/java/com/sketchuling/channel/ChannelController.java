20cd2b55c552a75d0e7c553e027839b8acb1188c
DESKTOP-0O2DKLO\comet <gukkyu.comet@gmail.com>
2025-02-07 02:34:34 +0900

    20250207

---
+++ src/main/java/com/sketchuling/channel/ChannelController.java
@@ -0,0 +1,26 @@
+package com.sketchuling.channel;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/channel")
+public class ChannelController {
+
+    @GetMapping("/")
+    public String channel(){
+        return "channel/channel";
+    }
+
+    @GetMapping("/specific")
+    public String specific(){
+        return "channel/specific";
+    }
+
+    @GetMapping("/chat")
+    public String chat(){
+        return "channel/chat";
+    }
+}
